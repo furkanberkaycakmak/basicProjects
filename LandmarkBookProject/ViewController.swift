2bb78721debe213bec23d3cac0748fe5098eeefa
furkanberkaycakmak <furkanberkaycakmak@icloud.com>
2021-11-07 06:36:54 +0300

    Initial Commit

---
+++ LandmarkBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandmarkBookProject
+//
+//  Created by Berkay ÇAKMAK on 7.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
